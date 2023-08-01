bbe71b17770e05603adec8214416b1512ab94393
EL Putnam <elputnam@inaction.ie>
2023-08-01 12:40:57 +0100

    initial commit

---
+++ sketch.js
@@ -0,0 +1,128 @@
+//Triple spiral set up for screenprinting
+
+// shapes
+let angle = 0;
+let radius = 0;
+let spot = 0;
+let radInc;
+let angInc;
+let tone = 100;
+let x;
+let y;
+let glitches = [];
+
+let cnv;
+
+function setup() {
+  cnv = createCanvas(windowHeight, windowHeight);
+
+  colorMode(HSB, 360, 100, 100, 100);
+  angleMode(DEGREES);
+  background(0);
+  frameRate(1);
+  rectMode(CENTER);
+  radInc = random(2);
+  angInc = random(5);
+  for (let i = 0; i < 20; i++){
+    glitches[i] = new Glitch(random(width), random(height), random(width), random(15,20));
+  }
+}
+
+function draw() {
+  background(255);
+  fill(0);
+  stroke(0);
+  //glitches
+  for(let i = 0; i < glitches.length; i++){
+    glitches[i].show();
+    if (frameCount%8==0){
+      glitches[i].change();
+    }
+  }
+
+  //TRiangle
+  push();
+  translate(width/2, height/2);
+  noFill();
+  // strokeWeight(random(1,10));
+  strokeWeight(10);
+  // stroke(180, random(100), tone);
+
+  triangle(random(-100, 100), -300, 300, random(100, 400), -300, random(100, 400));
+  pop();
+  
+
+  //Spirals
+  //change
+  radInc = random(2);
+  angInc = random(5);
+  //spiral 1
+  push();
+  translate(width/2, height/2-50);
+  spiral();
+  pop();
+
+  //spiral 2
+  push();
+  translate(width*.35, height/2+150);
+  spiral();
+  pop();
+
+  //spiral 3
+  push();
+  translate(width*.65, height/2+150);
+  spiral();
+  pop();
+
+}
+  
+function spiral(){
+  for (let i = 0; i < 200; i++){
+  //   radInc = random(2);
+  // angInc = random(5);
+    push();
+    rotate(angle);
+    angle += angInc;
+    radius += radInc;
+    // stroke(random(300, 360), 100, tone);
+    strokeWeight(random(5,15));
+    
+    // ellipse(radius,spot,random(5,10), random(5,10));
+    point(radius,spot);
+    pop();
+    if (radius>=10){
+      radius = random(-width*.2, width*.2);
+      spot = random(-width*.2, width*.2)
+      tone = random(100)
+    }
+  }
+}
+
+class Glitch{
+  constructor(x, y, wid, high){
+    this.x = x;
+    this.y = y;
+    this.wid = wid;
+    this.high = high;
+  }
+  change(){
+    this.x = random(width);
+    this.y = random(height);
+    this.wid = random(width);
+    this.high = random(10,20);
+  }
+  show(){
+    //noStroke();
+    //noFill();
+    // stroke(70, 100, 100);
+    // fill(70, 100, 100, random(100));
+    rect(this.x, this.y, this.wid, this.high);
+  }
+}
+
+
+function keyPressed(){
+  if (key == 'c' || key == 'C') {
+    saveCanvas("Morrigan", 'png');
+  }
+}
